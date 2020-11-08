f8905e6631f07938e0ff2149e7690721efdffd86
ghadir <ghadiraljafen@gmail.com>
2020-11-09 00:37:13 +0300

    Recommender is working now!

--- foodRecommender.py
+++ foodRecommender.py
@@ -1,13 +1,5 @@
 import pandas as pd
-patients = pd.read_csv('patientsCSV.csv', sep=',', encoding='latin-1',
-                    usecols=['Id', 'Diet'])
-food = pd.read_csv('FoodTest.csv', sep=',', encoding='latin-1',
-                     usecols=['FoodId', 'Name', 'Diet'])
-
-# Check the top 5 rows
-print(patients.head())
-# Check the file info
-print(patients.info())
+food = pd.read_csv('FoodDataset.csv', sep=',', encoding='latin-1', usecols=['FoodId', 'Food', 'Diet','Type','Calories', 'Serving'])
 # Check the top 5 rows
 print(food.head())
 # Check the file info
@@ -16,16 +8,15 @@ print(food.info())
 
 # Make a census of the diet keywords
 diet_labels = set()
-for s in food['Diet'].str.split(',').values:
+for s in food['Diet'].str.split('|').values:
     diet_labels = diet_labels.union(set(s))
 
 
 #Content based
 # Break up the big diet string into a string array
-food['Diet'] = food['Diet'].str.split(',')
+food['Diet'] = food['Diet'].str.split('|')
 # Convert diet to string value
 food['Diet'] = food['Diet'].fillna("").astype('str')
-
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 tf = TfidfVectorizer(analyzer='char',ngram_range=(1, 2),min_df=0, stop_words='english')
@@ -35,8 +26,9 @@ from sklearn.metrics.pairwise import linear_kernel
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 cosine_sim[:4, :4]
 # Build a 1-dimensional array with food names
-titles = food['Name']
-indices = pd.Series(food.index, index=food['Diet'])
+titles = food['Food']
+indices = pd.Series(food.index, index=food['Food'])
+print(indices)
 # Function that get food recommendations based on the cosine similarity score of food diet
 def food_recommendations(diet):
     idx = indices[diet]
@@ -46,4 +38,5 @@ def food_recommendations(diet):
     food_indices = [i[0] for i in sim_scores]
     return titles.iloc[food_indices]
 # Top 5 similar food:
-print (food_recommendations(1).head(5))
+print('Recommended Food:')
+print (food_recommendations(3).head(20))
